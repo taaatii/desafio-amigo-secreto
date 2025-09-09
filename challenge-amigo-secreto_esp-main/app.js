8c8703c17e32593988d87cf968f90749726daa12
Tatiana Charkowski <charkowskitati@gmail.com>
2025-09-09 00:40:10 -0300

    agrego funcion actualizarLista y mejora de sortearAmigo

--- challenge-amigo-secreto_esp-main/app.js
+++ challenge-amigo-secreto_esp-main/app.js
@@ -28,19 +28,34 @@ function actualizarLista() {
     // añadir los nombres en la app en un listado (elemento ul)
     }
 }
+function actualizarLista(){
+    let listaAmigos = document.getElementById('listaAmigos');
+    listaAmigos.innerHTML = ''; 
+    // bucle for para recorrer el array amigos y crear elementos de lista (<li>) para cada título.
+    for (let i = 0; i < ListaAmigos.length; i++) {
+        let li = document.createElement('li'); 
+        li.textContent = ListaAmigos[i];
+        listaAmigos.appendChild(li);
+    }
+}
 
 function sortearAmigo() {
+    if (ListaAmigos.length < 2) {
+        alert('Necesitas al menos dos amigos para hacer el sorteo.');
+        return;
+    }
     let indice = Math.floor(Math.random() * ListaAmigos.length); // Math.random() genera un número aleatorio entre 0 y 1, se multiplica por la longitud del array y se redondea hacia abajo con Math.floor()
     let amigo = ListaAmigos[indice]; // obtener el nombre del amigo seleccionado
+    AmigoSeleccionado = amigo;
 
     console.log('Amigo seleccionado:', amigo); // verificar el amigo seleccionado en la consola
+    
+    MostrarResultado(amigo);
 }
 function MostrarResultado() {
-    let AmigoSeleccionado = document.getElementById('resultado');
-    resultado.innerHTML = AsignarTextoElemento ('p',('Tu amigo secreto es:' + AmigoSeleccionado));
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<p> Tu amigo secreto es: ${AmigoSeleccionado} </p>`;
 }
-
 function LimpiarCaja() {
     document.getElementById('amigo').value = '';
 }
-
